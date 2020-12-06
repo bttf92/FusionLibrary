517b68d967c47bda71e7e3a89c97f0300a06cde8
bttf92 <lucadigiovanni@outlook.it>
2020-12-06 20:56:02 +0100

    Update SpawnLocation.cs

--- Prop/SpawnLocation.cs
+++ Prop/SpawnLocation.cs
@@ -1,28 +1,51 @@
-﻿using GTA;
+﻿using FusionLibrary.Extensions;
+using GTA;
 using GTA.Math;
+using GTA.Native;
 using System.Collections.Generic;
+using static FusionLibrary.Enums;
 
 namespace FusionLibrary
 {
+    public class SpawnLocationHandler 
+    {
+        public List<SpawnLocation> Locations { get; } = new List<SpawnLocation>();
+
+        public void Add(Vector3 position, bool direction)
+        {
+            Locations.Add(new SpawnLocation(this, position, direction));
+        }
+
+        public void Add(Vector3 position, Vector3 cameraPos, Vector3 cameraDir, bool direction = true)
+        {
+            Locations.Add(new SpawnLocation(this, position, cameraPos, cameraDir, direction));
+        }
+    }
+
     public class SpawnLocation
     {
-        public static List<SpawnLocation> SpawnLocations;
+        public Vector3 Position { get; }
+        public Vector3 CameraPos { get; } = Vector3.Zero;
+        public Vector3 CameraDir { get; } = Vector3.Zero;
+        public bool Direction { get; }
+        public string Name { get; }
 
-        public Vector3 Position;
-        public Vector3 CameraPos = Vector3.Zero;
-        public Vector3 CameraDir = Vector3.Zero;
-        public bool Direction;
-        public string Name;
+        private static Camera LocationCamera;
+        private SpawnLocationHandler SpawnLocationHandler;
 
-        public SpawnLocation(Vector3 position, bool direction)
+        public SpawnLocation(SpawnLocationHandler spawnLocationHandler, Vector3 position, bool direction)
         {
+            SpawnLocationHandler = spawnLocationHandler;
+
             Position = position;
             Direction = direction;
             Name = World.GetZoneLocalizedName(position);
         }
 
-        public SpawnLocation(Vector3 position, Vector3 cameraPos, Vector3 cameraDir, bool direction = true)
+        public SpawnLocation(SpawnLocationHandler spawnLocationHandler, Vector3 position, Vector3 cameraPos, Vector3 cameraDir, bool direction = true)
         {
+            SpawnLocationHandler = spawnLocationHandler;
+
             Position = position;
             Direction = direction;
             Name = World.GetZoneLocalizedName(position);
@@ -32,7 +55,39 @@ namespace FusionLibrary
 
         public override string ToString()
         {
-            return SpawnLocations.IndexOf(this).ToString();
+            return SpawnLocationHandler.Locations.IndexOf(this).ToString();
+        }
+
+        public void ShowLocation()
+        {
+            Function.Call(Hash.NEW_LOAD_SCENE_START_SPHERE, Position.X, Position.Y, Position.Z, 100, 0);
+
+            LocationCamera?.Delete();
+
+            if (CameraPos != Vector3.Zero && CameraDir != Vector3.Zero)
+            {
+                LocationCamera = World.CreateCamera(CameraPos, Vector3.Zero, 75);
+                LocationCamera.Direction = CameraDir;
+            }
+            else
+            {
+                LocationCamera = World.CreateCamera(Position.GetSingleOffset(Coordinate.Z, 10).GetSingleOffset(Coordinate.Y, 10), Vector3.Zero, 75);
+                LocationCamera.PointAt(Position);
+            }
+
+            World.RenderingCamera = LocationCamera;
+
+            Function.Call(Hash.LOCK_MINIMAP_POSITION, Position.X, Position.Y);
+        }
+
+        public static void ResetCamera()
+        {
+            LocationCamera?.Delete();
+
+            Function.Call(Hash.UNLOCK_MINIMAP_POSITION);
+
+            World.DestroyAllCameras();
+            World.RenderingCamera = null;
         }
     }
 }
