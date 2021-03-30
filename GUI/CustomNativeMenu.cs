99fa81483ed9b605af037b4461a1510ec756f4e4
bttf92 <lucadigiovanni@outlook.it>
2021-03-30 19:35:00 +0200

    Update CustomNativeMenu.cs

--- GUI/CustomNativeMenu.cs
+++ GUI/CustomNativeMenu.cs
@@ -4,6 +4,7 @@ using LemonUI.Elements;
 using LemonUI.Menus;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace FusionLibrary
 {
@@ -16,38 +17,7 @@ namespace FusionLibrary
     {
         public static ObjectPool ObjectPool { get; internal set; } = new ObjectPool();
 
-        public event OnItemActivated OnItemActivated;
-        public event OnItemSelected OnItemSelected;
-        public event OnItemCheckboxChanged OnItemCheckboxChanged;
-        public event OnItemValueChanged OnItemValueChanged;
-
         public static List<CustomNativeMenu> CustomNativeMenus = new List<CustomNativeMenu>();
-
-        public string InternalName { get; protected set; }
-
-        public CustomNativeMenu(string title) : base(title)
-        {
-            CustomNativeMenus.Add(this);
-            ObjectPool.Add(this);
-        }
-
-        public CustomNativeMenu(string title, string subtitle) : base(title, subtitle)
-        {
-            CustomNativeMenus.Add(this);
-            ObjectPool.Add(this);
-        }
-
-        public CustomNativeMenu(string title, string subtitle, string description) : base(title, subtitle, description)
-        {
-            CustomNativeMenus.Add(this);
-            ObjectPool.Add(this);
-        }
-
-        public CustomNativeMenu(string title, string subtitle, string description, I2Dimensional banner) : base(title, subtitle, description, banner)
-        {
-            CustomNativeMenus.Add(this);
-            ObjectPool.Add(this);
-        }
 
         internal static void TickAll()
         {
@@ -57,6 +27,55 @@ namespace FusionLibrary
                     x.Tick();
             });
         }
+
+        public event OnItemActivated OnItemActivated;
+        public event OnItemSelected OnItemSelected;
+        public event OnItemCheckboxChanged OnItemCheckboxChanged;
+        public event OnItemValueChanged OnItemValueChanged;
+
+        public string InternalName { get; protected set; }
+
+        private static I2Dimensional defaultBanner = new ScaledTexture(PointF.Empty, new SizeF(0, 108), "commonmenu", "interaction_bgd");
+
+        public CustomNativeMenu(string title) : this(title, "", "", defaultBanner)
+        {
+
+        }
+
+        public CustomNativeMenu(string title, string subtitle) : this(title, subtitle, "", defaultBanner)
+        {
+
+        }
+
+        public CustomNativeMenu(string title, string subtitle, string description) : this(title, subtitle, description, defaultBanner)
+        {
+
+        }
+
+        public CustomNativeMenu(string title, string subtitle, string description, I2Dimensional banner) : base(title, subtitle, description, banner)
+        {
+            CustomNativeMenus.Add(this);
+            ObjectPool.Add(this);
+
+            Shown += Menu_Shown;
+            Closing += Menu_Closing;
+            OnItemActivated += Menu_OnItemActivated;
+            OnItemSelected += Menu_OnItemSelected;
+            OnItemCheckboxChanged += Menu_OnItemCheckboxChanged;
+            OnItemValueChanged += Menu_OnItemValueChanged;
+        }
+
+        public abstract void Menu_OnItemValueChanged(NativeSliderItem sender, EventArgs e);
+
+        public abstract void Menu_OnItemCheckboxChanged(NativeCheckboxItem sender, EventArgs e, bool Checked);
+
+        public abstract void Menu_OnItemSelected(NativeItem sender, SelectedEventArgs e);
+
+        public abstract void Menu_OnItemActivated(NativeItem sender, EventArgs e);
+
+        public abstract void Menu_Closing(object sender, System.ComponentModel.CancelEventArgs e);
+
+        public abstract void Menu_Shown(object sender, EventArgs e);
 
         public new void Add(NativeItem nativeItem)
         {
@@ -141,5 +160,146 @@ namespace FusionLibrary
         }
 
         public abstract void Tick();
+
+        public abstract string GetMenuTitle();
+
+        public abstract string GetMenuDescription();
+
+        public abstract string GetItemTitle(string itemName);
+
+        public abstract string GetItemDescription(string itemName);
+
+        public abstract string GetItemValueTitle(string itemName, string valueName);
+
+        public abstract string GetItemValueDescription(string itemName, string valueName);
+
+        public string GetItemValueTitle(object sender, string valueName)
+        {
+            return GetItemValueTitle(((NativeItem)sender).Tag.ToString(), valueName);
+        }
+
+        public string[] GetItemValueTitle(string itemName, params string[] valueNames)
+        {
+            string[] ret = new string[valueNames.Length];
+
+            for (int i = 0; i < valueNames.Length; i++)
+                ret[i] = GetItemValueTitle(itemName, valueNames[i]);
+
+            return ret;
+        }
+
+        public string GetItemValueDescription(object sender, string valueName)
+        {
+            return GetItemValueDescription(((NativeItem)sender).Tag.ToString(), valueName);
+        }
+
+        public string[] GetItemValueDescription(string itemName, params string[] valueNames)
+        {
+            string[] ret = new string[valueNames.Length];
+
+            for (int i = 0; i < valueNames.Length; i++)
+                ret[i] = GetItemValueDescription(itemName, valueNames[i]);
+
+            return ret;
+        }
+
+        public NativeSubmenuItem NewSubmenu(NativeMenu menu, string menuName)
+        {
+            NativeSubmenuItem item = AddSubMenu(menu);
+            item.Title = GetItemTitle(menuName);
+            item.Description = GetItemDescription(menuName);
+
+            item.Tag = menuName;
+
+            return item;
+        }
+
+        public NativeCheckboxItem NewCheckboxItem(string itemName)
+        {
+            NativeCheckboxItem item;
+
+            Add(item = new NativeCheckboxItem(GetItemTitle(itemName), GetItemDescription(itemName)));
+
+            item.Tag = itemName;
+
+            return item;
+        }
+
+        public NativeCheckboxItem NewCheckboxItem(string itemName, bool isChecked)
+        {
+            NativeCheckboxItem item;
+
+            Add(item = new NativeCheckboxItem(GetItemTitle(itemName), GetItemDescription(itemName), isChecked));
+
+            item.Tag = itemName;
+
+            return item;
+        }
+
+        public NativeListItem<T> NewListItem<T>(string itemName, params T[] itemValues)
+        {
+            NativeListItem<T> item;
+
+            Add(item = new NativeListItem<T>(GetItemTitle(itemName), GetItemDescription(itemName), itemValues));
+
+            item.Tag = itemName;
+
+            return item;
+        }
+
+        public NativeListItem<string> NewLocalizedListItem(string itemName, params string[] itemValues)
+        {
+            NativeListItem<string> item;
+
+            Add(item = new NativeListItem<string>(GetItemTitle(itemName), GetItemDescription(itemName), GetItemValueTitle(itemName, itemValues)));
+
+            item.Tag = itemName;
+
+            return item;
+        }
+
+        public NativeListItem<T> NewListItem<T>(string itemName)
+        {
+            NativeListItem<T> item;
+
+            Add(item = new NativeListItem<T>(GetItemTitle(itemName), GetItemDescription(itemName)));
+
+            item.Tag = itemName;
+
+            return item;
+        }
+
+        public NativeSliderItem NewSliderItem(string itemName, int max, int value)
+        {
+            NativeSliderItem item;
+
+            Add(item = new NativeSliderItem(GetItemTitle(itemName), GetItemDescription(itemName), max, value));
+
+            item.Tag = itemName;
+
+            return item;
+        }
+
+        public NativeItem NewItem(string itemName)
+        {
+            NativeItem item;
+
+            Add(item = new NativeItem(GetItemTitle(itemName), GetItemDescription(itemName)));
+
+            item.Tag = itemName;
+
+            return item;
+        }
+
+        public NativeItem NewItem(int pos, string itemName)
+        {
+            NativeItem item;
+
+            Add(pos, item = new NativeItem(GetItemTitle(itemName), GetItemDescription(itemName)));
+
+            item.Tag = itemName;
+
+            return item;
+        }
     }
 }
