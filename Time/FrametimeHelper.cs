df3f57922bfa23f4bf2cdc4201abc787bbbfe80c
bttf92 <lucadigiovanni@outlook.it>
2021-06-04 20:58:18 +0200

    Added FrametimeHelper
    Update FrametimeHelper.cs
    
    Update FrametimeHelper.cs
    
    Update FrametimeHelper.cs

---
+++ Time/FrametimeHelper.cs
@@ -0,0 +1,43 @@
+﻿using GTA;
+using System;
+
+namespace FusionLibrary
+{
+    public class FrametimeHelper
+    {
+        private double remainder;
+        public float BaseFPS { get; }
+
+        public int Count { get; private set; }
+
+        public FrametimeHelper(float fps)
+        {
+            BaseFPS = fps;
+
+            Reset();
+        }
+
+        public void Reset()
+        {
+            remainder = 0;
+            Count = 0;
+        }
+
+        public void Tick()
+        {
+            float delta = BaseFPS / Game.FPS;
+
+            Count = (int)Math.Truncate(delta);
+
+            remainder += delta - Count;
+
+            if (remainder >= 1)
+            {
+                int _remainder = (int)Math.Truncate(remainder);
+
+                Count += _remainder;
+                remainder -= _remainder;
+            }
+        }
+    }
+}
