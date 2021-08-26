2bb7d62b8e0af959e57d1ac99b880dd00255b259
bttf92 <lucadigiovanni@outlook.it>
2021-08-26 15:46:50 +0200

    Added documentation for interaction system

--- Prop/InteractionController.cs
+++ Prop/InteractionController.cs
@@ -16,14 +16,29 @@ namespace FusionLibrary
                 GlobalInteractionControllerList[i].Tick();
         }
 
+        /// <summary>
+        /// This event is invoked when a <see cref="InteractionProp"/> is completed.
+        /// </summary>
         public event EventHandler<InteractionProp> OnInteractionComplete;
 
+        /// <summary>
+        /// List of <see cref="InteractionProp"/>.
+        /// </summary>
         public List<InteractionProp> InteractionProps { get; } = new List<InteractionProp>();
 
+        /// <summary>
+        /// Index of the selected <see cref="InteractionProp"/>.
+        /// </summary>
         public int CurrentInteractionIndex { get; private set; } = -1;
 
+        /// <summary>
+        /// Returns true if this <see cref="InteractionController"/> is playing.
+        /// </summary>
         public bool IsPlaying { get; private set; }
 
+        /// <summary>
+        /// Returns the selected <see cref="InteractionProp"/>.
+        /// </summary>
         public InteractionProp CurrentInteractionProp
         {
             get
@@ -35,6 +50,9 @@ namespace FusionLibrary
             }
         }
 
+        /// <summary>
+        /// ID for hover <see cref="InteractionProp"/>.
+        /// </summary>
         private int _hoverId = -1;
 
         public InteractionController()
@@ -42,6 +60,23 @@ namespace FusionLibrary
             GlobalInteractionControllerList.Add(this);
         }
 
+        /// <summary>
+        /// Creates a new <see cref="InteractionProp"/>.
+        /// </summary>
+        /// <param name="model"><see cref="CustomModel"/> to be used for the <see cref="AnimateProp"/>.</param>
+        /// <param name="entity"><see cref="Entity"/> at which the <see cref="AnimateProp"/>.</param>
+        /// <param name="boneName">Bone name of <paramref name="entity"/> used for attach.</param>
+        /// <param name="interactionType"><see cref="InteractionType"/> for this <see cref="InteractionProp"/>.</param>
+        /// <param name="movementType"><see cref="AnimationType"/> for this <see cref="InteractionProp"/>.</param>
+        /// <param name="coordinateInteraction"><see cref="Coordinate"/> of the axis</param>
+        /// <param name="control"><see cref="Control"/> which fires up this <see cref="InteractionProp"/>.</param>
+        /// <param name="invert">Inverts the reading of the <paramref name="control"/> value.</param>
+        /// <param name="min">Minimum value.</param>
+        /// <param name="max">Maximum value.</param>
+        /// <param name="startValue">Starting value.</param>
+        /// <param name="step">Step value.</param>
+        /// <param name="sensitivityMultiplier">Sensitivity multiplier for <paramref name="control"/> value.</param>
+        /// <returns>New instance of <see cref="InteractionProp"/>.</returns>
         public InteractionProp Add(CustomModel model, Entity entity, string boneName, InteractionType interactionType, AnimationType movementType, Coordinate coordinateInteraction, Control control, bool invert, int min, int max, float startValue = 0f, float step = 1f, float sensitivityMultiplier = 1f)
         {
             InteractionProp interactionProp;
--- Prop/InteractionProp.cs
+++ Prop/InteractionProp.cs
@@ -7,23 +7,44 @@ namespace FusionLibrary
 {
     public class InteractionProp
     {
+        /// <summary>
+        /// This event is invoked when the <see cref="InteractionProp"/> is completed.
+        /// </summary>
         public event EventHandler<InteractionProp> OnInteractionComplete;
 
         /// <summary>
-        /// Interactable prop.
+        /// Interactable <see cref="FusionLibrary.AnimateProp"/>.
         /// </summary>
         public AnimateProp AnimateProp { get; }
 
+        /// <summary>
+        /// Interaction type of the <see cref="AnimateProp"/>.
+        /// </summary>
         public InteractionType InteractionType { get; }
 
+        /// <summary>
+        /// Control used for this <see cref="InteractionProp"/>.
+        /// </summary>
         public Control Control { get; }
 
+        /// <summary>
+        /// Animation type of the <see cref="InteractionProp"/>.
+        /// </summary>
         public AnimationType MovementType { get; }
 
+        /// <summary>
+        /// Axis for the <see cref="AnimateProp"/> interaction.
+        /// </summary>
         public Coordinate CoordinateInteraction { get; }
 
+        /// <summary>
+        /// Shortcut for <see cref="FusionLibrary.CoordinateSetting"/> of <see cref="AnimateProp"/>.
+        /// </summary>
         private CoordinateSetting CoordinateSetting => AnimateProp[MovementType][AnimationStep.First][CoordinateInteraction];
 
+        /// <summary>
+        /// Current value of <see cref="CoordinateInteraction"/> of the <see cref="AnimateProp"/>.
+        /// </summary>
         public float CurrentValue
         {
             get
@@ -39,8 +60,14 @@ namespace FusionLibrary
             }
         }
 
+        /// <summary>
+        /// ID of this instance in the <see cref="InteractionController"/>.
+        /// </summary>
         public int ID => _controller.InteractionProps.IndexOf(this);
 
+        /// <summary>
+        /// Returns true if this <see cref="AnimateProp"/> is interaction mode.
+        /// </summary>
         public bool IsPlaying { get; private set; }
 
         /// <summary>
@@ -48,10 +75,33 @@ namespace FusionLibrary
         /// </summary>
         private bool _invert;
 
+        /// <summary>
+        /// Sensitivity modifier.
+        /// </summary>
         private float _sensitivity = 14;
 
+        /// <summary>
+        /// Owner of this <see cref="InteractionProp"/>.
+        /// </summary>
         private InteractionController _controller;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="model"></param>
+        /// <param name="entity"></param>
+        /// <param name="boneName"></param>
+        /// <param name="interactionType"></param>
+        /// <param name="movementType"></param>
+        /// <param name="coordinateInteraction"></param>
+        /// <param name="control"></param>
+        /// <param name="invert"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <param name="startValue"></param>
+        /// <param name="step"></param>
+        /// <param name="sensitivityMultiplier"></param>
         internal InteractionProp(InteractionController controller, CustomModel model, Entity entity, string boneName, InteractionType interactionType, AnimationType movementType, Coordinate coordinateInteraction, Control control, bool invert, int min, int max, float startValue, float step, float sensitivityMultiplier)
         {
             InteractionType = interactionType;
