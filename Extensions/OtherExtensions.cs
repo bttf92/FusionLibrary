99c60020e847cc163b85a25b3e8c3fc4a6a9d331
bttf92 <lucadigiovanni@outlook.it>
2021-11-09 09:12:17 +0100

    Added Next enum func

--- Extensions/OtherExtensions.cs
+++ Extensions/OtherExtensions.cs
@@ -88,6 +88,22 @@ namespace FusionLibrary.Extensions
         }
 
         /// <summary>
+        /// Gets next value of enum.
+        /// </summary>
+        /// <typeparam name="T">Enum type.</typeparam>
+        /// <param name="src">Enum.</param>
+        /// <returns>Next value of enum.</returns>
+        public static T Next<T>(this T src) where T : struct
+        {
+            if (!typeof(T).IsEnum)
+                throw new ArgumentException(string.Format("Argument {0} is not an Enum", typeof(T).FullName));
+
+            T[] Arr = (T[])Enum.GetValues(src.GetType());
+            int j = Array.IndexOf<T>(Arr, src) + 1;
+            return (Arr.Length == j) ? Arr[0] : Arr[j];
+        }
+
+        /// <summary>
         /// Returns a random element from an <see cref="IEnumerable{T}"/>.
         /// </summary>
         /// <typeparam name="T"><see cref="Type"/> of the element.</typeparam>
