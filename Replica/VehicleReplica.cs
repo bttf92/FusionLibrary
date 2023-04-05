bd0a8b164f939d58a74f87d41c04fcf062cc0148
nomakewan <nomakewan@users.noreply.github.com>
2023-04-04 20:00:58 -0700

    Add FuelLevel to VehicleReplica
    Added FuelLevel to VehicleReplica.

--- Replica/VehicleReplica.cs
+++ Replica/VehicleReplica.cs
@@ -15,6 +15,7 @@ namespace FusionLibrary
         public float EngineHealth { get; }
         public float EnginePowerMultiplier { get; }
         public bool EngineRunning { get; }
+        public float FuelLevel { get; }
         public int HighGear { get; }
         public bool Turbo { get; }
         public bool TireSmoke { get; }
@@ -51,6 +52,7 @@ namespace FusionLibrary
             EngineHealth = vehicle.EngineHealth;
             EnginePowerMultiplier = vehicle.EnginePowerMultiplier;
             EngineRunning = vehicle.IsEngineRunning;
+            FuelLevel = vehicle.FuelLevel;
             HighGear = vehicle.HighGear;
             TireSmoke = vehicle.Mods[VehicleToggleModType.TireSmoke].IsInstalled;
             Turbo = vehicle.Mods[VehicleToggleModType.Turbo].IsInstalled;
@@ -206,6 +208,7 @@ namespace FusionLibrary
                 }
             }
 
+            vehicle.FuelLevel = FuelLevel;
             vehicle.IsEngineRunning = EngineRunning;
 
             vehicle.CurrentRPM = RPM;
