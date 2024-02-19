d73f17347a408b216e3c0d6d878c10cb99b2da6f
nomakewan <nomakewan@users.noreply.github.com>
2024-02-19 07:25:33 -0800

    Restore old model check
    Restored the old model check code as it works more efficiently; once SHVDN 3.7.0 releases with its more complex memory scanner this can be obsolete, but for now it is best to keep it as-is.

--- Main.cs
+++ Main.cs
@@ -2,7 +2,6 @@
 using GTA.Native;
 using System;
 using System.Linq;
-using System.Runtime.InteropServices;
 
 namespace FusionLibrary
 {
@@ -28,35 +27,18 @@ namespace FusionLibrary
 
             if (FusionUtils.FirstTick)
             {
-                // Bypass model checks
-                IntPtr addr = Game.FindPattern("74 12 48 8B 10 48 8B C8 FF 52 30 84 C0 74 05 48 8B C3");
-                addr = addr != IntPtr.Zero ? (addr + 11) : IntPtr.Zero;
+                // Bypass models check
+                IntPtr addr = Game.FindPattern("48 85 C0 0F 84 ? ? ? ? 8B 48 50");
 
-                unsafe
+                if (addr != IntPtr.Zero)
                 {
-                    if (addr != IntPtr.Zero && *(byte*)addr != 0x90)
+                    for (int i = 0; i < 24; i++)
                     {
-                        const int bytesToWriteInstructions = 4;
-                        byte[] nopBytes = Enumerable.Repeat((byte)0x90, bytesToWriteInstructions).ToArray();
-                        Marshal.Copy(nopBytes, 0, addr, bytesToWriteInstructions);
-                    }
-                }
-
-                // Bypass prop checks
-                addr = Game.FindPattern("40 84 ?? 74 13 E8 ?? ?? ?? ?? 48 85 C0 75 09 38 45 57 0F 84");
-
-                unsafe
-                {
-                    if (addr != IntPtr.Zero)
-                    {
-                        addr = Game.FindPattern("33 C1 48 8D 4D 6F", new IntPtr((byte*)addr + 0x3A));
-                        addr = addr != IntPtr.Zero ? (addr + 0x16) : IntPtr.Zero;
-
-                        if (addr != IntPtr.Zero && *(byte*)addr != 0x90)
+                        unsafe
                         {
-                            const int bytesToWriteInstructions = 0x18;
-                            byte[] nopBytes = Enumerable.Repeat((byte)0x90, bytesToWriteInstructions).ToArray();
-                            Marshal.Copy(nopBytes, 0, addr, bytesToWriteInstructions);
+                            byte* val = (byte*)(addr + i);
+
+                            *val = 0x90;
                         }
                     }
                 }
